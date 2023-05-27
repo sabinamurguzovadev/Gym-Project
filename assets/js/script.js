ccd222497ea4009fbdacb8bcc2c4c23653ac5a09
Sabina Murguzova <120595196+sabinamurguzovadev@users.noreply.github.com>
2023-05-26 21:48:11 -0700

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+// let BASE_URL = ` http://localhost:8080/users`
+// let row = document.querySelector('.icons')
+
+
+
+// axios(BASE_URL).then((res => allData(res.data)))
+// function allData(data) {
+//     console.log(data);
+//     row.innerHTML = ""
+//     data.forEach(element => {
+//         row.innerHTML += `
+//         <div class="col-md-4">
+//         <div class="icons-textbox">
+//             <img src="${element.photo}" alt="">
+//             <p>${element.description}</p>
+//         </div> 
+             
+        
+        
+//         `
+//     });
+
+
+
+// }
